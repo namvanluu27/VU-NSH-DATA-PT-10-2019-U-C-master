1214c5bc5067ecd29dfb4a312487e2d5d0ae9353
ZeroK19 <namvanluu27@gmail.com>
2020-03-05 15:06:17 -0600

    class activity

--- 15-Interactive-Visualizations-and-Dashboards/Day 1 (Thursday)/Activities/07-Ins_Multi_Trace/Solved/plots.js
+++ 15-Interactive-Visualizations-and-Dashboards/Day 1 (Thursday)/Activities/07-Ins_Multi_Trace/Solved/plots.js
@@ -1,33 +1,29 @@
-/**
- * Generates an array of random numbers between 0 and 9
- * @param {integer} n: length of array to generate
- */
-function randomNumbersBetween0and9(n) {
-  var randomNumberArray = [];
-  for (var i = 0; i < n; i++) {
-    var randomNumber = Math.floor(Math.random() * 10);
-    randomNumberArray.push(randomNumber);
-  }
-  return randomNumberArray;
-}
 
-// Create our first trace
-var trace1 = {
-  x: [1, 2, 3, 4, 5],
-  y: randomNumbersBetween0and9(5),
-  type: "scatter"
+
+var greekTrace = {
+  x: data.map(god => god.pair),
+  y: data.map(god => god.greekSearchResults),
+  type: "bar",
+  name: "Greek"
 };
 
-// Create our second trace
-var trace2 = {
-  x: [1, 2, 3, 4, 5],
-  y: randomNumbersBetween0and9(5),
-  type: "scatter"
+
+
+var romanTrace = {
+  x: data.map(god => god.pair),
+  y: data.map(god => god.romanSearchResults),
+  type: "bar",
+  name: "Roman"
 };
 
 // The data array consists of both traces
-var data = [trace1, trace2];
+var data = [greekTrace, romanTrace];
+
+var layout = {
+  title: "Greek vs Roman Gods",
+  barmode: "group",
+};
 
 // Note that we omitted the layout object this time
 // This will use default parameters for the layout
-Plotly.newPlot("plot", data);
+Plotly.newPlot("plot", data, layout);
--- 15-Interactive-Visualizations-and-Dashboards/Day 2 (Saturday)/Activities/06-Stu_Sort_Slice/Unsolved/sliceSort.js
+++ 15-Interactive-Visualizations-and-Dashboards/Day 2 (Saturday)/Activities/06-Stu_Sort_Slice/Unsolved/sliceSort.js
@@ -2,18 +2,26 @@
 numArray = [9.9, 6.1, 17.1, 22.7, 4.6, 8.7, 7.2];
 
 // Sort the array in descending order and assign the results to a variable
-
+var descSort = numArray.sort(function sortDescFunction(a, b) {
+  return b - a;
+});
 
 // Print the results to the console
+console.log(descSort);
 
 // Sort the array in descending order using an arrow function
 // and assign the results to a variable and print to the console
-
+var descArrowSort = numArray.sort((a, b) => b - a);
+console.log(descArrowSort);
 
 // Reverse the array order
-
+var reversedSort = descArrowSort.reverse();
+console.log(reversedSort);
 
 // Sort the array in ascending order using an arrow function
-
+var ascendSort = numArray.sort((a, b) => a - b);
+console.log(ascendSort);
 
 // Slice the first five elements of the sortedAscending array, assign to a variable
+var slicedArray = ascendSort.slice(0, 5);
+console.log(slicedArray);
